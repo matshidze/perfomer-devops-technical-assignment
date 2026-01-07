79fe0298b09b76f112d4a55b727f0585b442e991
Rembu <machidze@gmail.com>
2026-01-07 21:09:29 +0200

    fixing security error

--- app/app.py
+++ app/app.py
@@ -128,6 +128,4 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    host = os.getenv("APP_HOST", "0.0.0.0")
-    port = int(os.getenv("APP_PORT", "5000"))
-    app.run(host=host, port=port)
+   app.run(host="0.0.0.0", port=5000)
